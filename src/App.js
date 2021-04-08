b22758c72598b93569abfb0044a91700be893aa8
gunnerhowe <gunnerlevihowe@gmail.com>
2021-04-08 17:04:37 -0500

    more files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from 'C:\\Users\\gunne\\react-website-project\\src\\components\\pages\\Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Tech from 'C:\\Users\\gunne\\react-website-project\\src\\components\\pages\\Tech';
+import Contact from 'C:\\Users\\gunne\\react-website-project\\src\\components\\pages\\Contact';
+import SignUp from 'C:\\Users\\gunne\\react-website-project\\src\\components\\pages\\SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/tech' component={Tech} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
